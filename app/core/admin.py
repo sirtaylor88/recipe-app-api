f0f21dfe86101c26d5f358d36166760c1027e61d
Nhat Tai NGUYEN <nhattai.nguyen88@gmail.com>
2022-01-29 08:56:37 +0100

    Registered Ingrendient to admin.

--- app/core/admin.py
+++ app/core/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.translation import gettext as _
 
-from core.models import User, Tag
+from core.models import User, Tag, Ingredient
 
 
 class UserAdmin(BaseUserAdmin):
@@ -52,3 +52,4 @@ class UserAdmin(BaseUserAdmin):
 admin.site.register(User, UserAdmin)
 # Use the default Admin
 admin.site.register(Tag)
+admin.site.register(Ingredient)
